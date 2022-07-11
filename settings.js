0d2f80addcaa9f23ad08b8ac82dd5dd1dedfea89
sanchnaya <109079130+sanchnaya@users.noreply.github.com>
2022-07-11 18:13:56 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
+global.owner = ['94762591630']
+global.premium = ['94762591630']
+global.ownernomer = '9476291630'
 global.ownername = '🐦Star Boy Nexus'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
